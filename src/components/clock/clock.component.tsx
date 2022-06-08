3317a2fb4106ac4c781553059a2f582ded71ef02
尹山 <yin_s@hdec.com>
2022-06-08 21:46:57 +0800

    feat: Update Clock Animation

--- src/components/clock/clock.component.tsx
+++ src/components/clock/clock.component.tsx
@@ -24,31 +24,73 @@ for (let i = 1; i < 13; i++) {
   })
 }
 
+const clockProps = {
+  modelValue: {
+    default: () => new Date(),
+    required: false,
+    type: [Date, Number, String]
+  }
+}
+
 export default defineComponent({
   name: clock.name,
-  setup() {
-    const hour: Ref<ClockHour | ClockHourExtra> = ref(5)
-    const minute: Ref<number> = ref(43)
-    const second: Ref<number> = ref(21)
+  props: clockProps,
+  setup(props) {
+    const moment: Date = new Date(props.modelValue)
+    const hour: Ref<number> = ref(moment.getHours())
+    const minute: Ref<number> = ref(moment.getMinutes())
+    const second: Ref<number> = ref(moment.getSeconds())
+    let timer: NodeJS.Timer | null = null
 
-    const resolveAngle = (value: ClockHour | ClockHourExtra): Style => {
-      // deg = (value % 12) / 12 * 360
-      const deg: number = (value % 12) * 30
+    const dispose = (): void => {
+      timer && clearTimeout(timer)
+    }
+
+    const resolveAngle = (value: number, base: 12 | 60): Style => {
+      // deg = value / base * 360
+      const deg: number = value / base * 360
       return {
         transform: `rotate(${deg}deg)`
       }
     }
 
+    const resolveLabel = (value: number): string => {
+      return value < 10 ? `0${value}` : `${value}`
+    }
+
+    const resolveClock = (): void => {
+      timer = setInterval(() => {
+        moment.setSeconds(moment.getSeconds() + 1)
+        updateClock()
+      }, 1000)
+    }
+
+    const updateClock = (): void => {
+      hour.value = moment.getHours()
+      minute.value = moment.getMinutes()
+      second.value = moment.getSeconds()
+    }
+
     return {
+      dispose,
       hour,
       minute,
       resolveAngle,
+      resolveLabel,
+      resolveClock,
       second
     }
   },
   render() {
     return (
       <div class={clock.bem()}>
+        <div class={clock.bem('field')}>
+          <span>{this.resolveLabel(this.hour)}</span>
+          <span>:</span>
+          <span>{this.resolveLabel(this.minute)}</span>
+          <span>:</span>
+          <span>{this.resolveLabel(this.second)}</span>
+        </div>
         <ul class={clock.bem('dial')}>
           {
             CLOCK_HOUR_TICKS.map(({ legend, name }) => (
@@ -57,11 +99,17 @@ export default defineComponent({
           }
         </ul>
         <ul class={clock.bem('hands')}>
-          <li class={clock.bem('hour-hand')} />
-          <li class={clock.bem('minute-hand')} />
-          <li class={clock.bem('second-hand')} />
+          <li class={clock.bem('hour-hand')} style={this.resolveAngle(this.hour, 12)} />
+          <li class={clock.bem('minute-hand')} style={this.resolveAngle(this.minute, 60)} />
+          <li class={clock.bem('second-hand')} style={this.resolveAngle(this.second, 60)} />
         </ul>
       </div>
     )
+  },
+  mounted() {
+    this.resolveClock()
+  },
+  destroyed() {
+    this.dispose()
   }
 })
